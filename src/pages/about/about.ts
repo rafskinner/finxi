9870290608cd0b591e3fc0d023ba5444aaf4142a
rafskinner <rafaelskinner@hotmail.com>
2017-07-29 17:21:51 -0300

    Implementacao basica das abas

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,8 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+  tables = [
+    {id: 6, label: "Mesa 6", available: false, order: ""},
+    {id: 7, label: "Mesa 7", available: false, order: ""},
+    {id: 8, label: "Mesa 8", available: false, order: ""}
+  ];
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  itemTapped($event, table) {
+    let tappedTable = this.tables.find(obj => obj.id === table.id);
+    tappedTable.available = true;
+    console.log("liberando", tappedTable);
   }
 
 }
